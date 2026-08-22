500f7db8e89aff9c583766352129df8c152c923c
CandyDevxOfficial <parrichat.may.25@gmail.com>
2026-08-22 22:08:18 +0700

    Fix formatting by adding newline at end of file

--- server.js
+++ server.js
@@ -448,4 +448,4 @@ app.get('/Scripts', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
-                              
+        
